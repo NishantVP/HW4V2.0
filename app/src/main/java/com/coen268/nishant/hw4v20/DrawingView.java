3cbfe4c60d35045cf6caea133c98c2afb5469db9
Nishant Phatangare <nishant1914@gmail.com>
2015-02-14 18:55:29 -0800

    Tut Part 1

---
+++ app/src/main/java/com/coen268/nishant/hw4v20/DrawingView.java
@@ -0,0 +1,21 @@
+package com.coen268.nishant.hw4v20;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by Nishant on 2/14/2015.
+ */
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //get drawing area setup for interaction
+    }
+
+}
